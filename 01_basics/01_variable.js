c0b50a13f999624c2f2789f51ddd6b758bc62a76
Naval-Bisht <112773798+Naval-Bisht@users.noreply.github.com>
2026-02-26 13:22:00 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const accountId =10;
+let accountEmail = "anva@gmai.com";
+var accountPassword = "12345";
+accountCity ="jaipur" // can declare a
+let accountState;
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict";  // for newer strict
+// alert(3+3)   // this will give the pop-up in the window in broweset but in the termial it will give error as now it not used any more;
+let a=10;
+
+console.log(typeof null);  // obect as output 
+console.log(typeof undefined);   // undefined is the ans
+

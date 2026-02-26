7308a5baa94873ab864c12c692533429b16d5f15
Naval-Bisht <112773798+Naval-Bisht@users.noreply.github.com>
2026-02-26 13:45:47 +0000

    variable

---
+++ 01_basics/03_conversionproblem.js
@@ -0,0 +1,27 @@
+let score =null;
+
+
+// console.log(typeof score);
+
+let valInNumber = Number(score);
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+// let a = undefined;
+// let b= String(a);
+// console.log(b);
+// console.log(typeof b);
+
+/******************* Operations.*********** */
+
+
+let a = 10;
+let b= -a;
+console.log(b);
+
+
